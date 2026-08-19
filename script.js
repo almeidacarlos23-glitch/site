c4618d248d7627fcbdc4cd04c7a3aec832ad3b2c
almeidacarlos23-glitch <almeida.carlos23@escola.pr.gov.br>
2026-08-19 10:19:58 +0000

    4

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-const  botão = document.getElementById('btnComparar');
+const  botão = document.getElementById("como");
+
 botão.addEventListener('click', function() {
-    window.location.href = 'pagina-comparação.html';
-});
+    const titulo = document.querySelector("h1");
+    const texto = document. querySelector("p");
+
+    titulo.textContent = "Comparativo: GTA 6 x read dead 2";
+    texto.textContent = ("o GTA 6 é um dos jogos mais agurdados da historia. Agora podemos comparar sua pré venda com outro grandande lançamento")
+
