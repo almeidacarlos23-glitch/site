625e23c3c3dcda271802701c59fc1fdfb206059f
almeidacarlos23-glitch <almeida.carlos23@escola.pr.gov.br>
2026-08-19 11:14:32 +0000

    6

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ botão.addEventListener('click', function() {
     const texto = document. querySelector("p");
 
     titulo.textContent = "Comparativo: GTA 6 x read dead 2";
-    texto.textContent = ("o GTA 6 é um dos jogos mais agurdados da historia. Agora podemos comparar sua pré venda com outro grandande lançamento")
+    texto.textContent = ("GTA 6 pré-vendas vs. Red Dead Redemption 2, GTA 6: a Take-Two registrou aproximadamente US$ 1,39 bilhão em Net Bookings no trimestre em que abriu as pré-vendas, RDR2 já havia gerado US$ 725 milhões somente nos primeiros três dias de lançamento, Conclusão: mesmo usando uma estimativa conservadora para RDR2, as pré-vendas de GTA 6 em poucos dias já estão na mesma ordem de grandeza que todo o primeiro ano de vendas de RDR2. E isso acontece antes do lançamento de GTA 6, marcado para 19 de novembro de 2026.")
+    
 })
+
